22706af12dbb667c850d951f0cfa9f7718c82061
bradford roarr <bradford.barr@gmail.com>
2023-04-13 16:34:43 -0400

    double tap esc

---
+++ keyboards/keychron/v4/ansi/keymaps/broarr/config.h
@@ -0,0 +1,5 @@
+#define TAPPING_TERM 300 // speed of double tap in ms
+
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#define RGB_MATRIX_KEYPRESSES // reacts to keypresses
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_SOLID_REACTIVE // Loads the default into eeprom
---
+++ keyboards/keychron/v4/ansi/keymaps/broarr/rules.mk
@@ -0,0 +1,4 @@
+# Build Options
+#   change yes to no to disable.
+#
+TAP_DANCE_ENABLE = yes
