d51826fb38f2ea65caa9076171299964fc880745
bradford roarr <bradford_roarr@brown.edu>
2024-07-08 10:39:38 -0400

    fix: fix up the nav keys

--- keyboards/zsa/voyager/keymaps/broarr/keymap.c
+++ keyboards/zsa/voyager/keymaps/broarr/keymap.c
@@ -60,8 +60,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT(
         KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5, KC_6, KC_7,           KC_8,         KC_9,         KC_0,         KC_MINS,
-        CW_TOGG, KC_Q,         KC_W,         KC_F,         KC_P,         KC_B, KC_J, KC_L,           KC_U,         KC_Y,         KC_SCLN,      KC_BSLS,
-        KC_LCTL, LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N),   RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
+        KC_TAB, KC_Q,         KC_W,         KC_F,         KC_P,         KC_B, KC_J, KC_L,           KC_U,         KC_Y,         KC_SCLN,      KC_BSLS,
+        CW_TOGG, LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N),   RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
         KC_GRV,  KC_Z,         KC_X,         KC_C,         KC_D,         KC_V, KC_K, KC_H,           KC_COMMA,     KC_DOT,       KC_SLSH,      KC_EQL,
                                              LT(2,KC_ENT), LT(1,KC_TAB),            LT(4,KC_BSPC), LT(3,KC_SPC)
     ),
@@ -150,9 +150,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT(
         RGB_TOG, QK_KB,   RGB_MOD, RGB_M_P, RGB_VAD, RGB_VAI,                      _______, _______, _______, _______, _______, QK_BOOT,
-        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
-        _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
-        _______, _______, _______, _______, _______, _______,                      _______, C(S(KC_TAB)), C(KC_TAB), _______, _______, _______,
+        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      _______, KC_HOME, KC_PGUP, _______,  _______, _______,
+        _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
+        _______, _______, _______, _______, _______, _______,                      _______, KC_END,  KC_PGDN, _______, _______, _______,
                                                      _______, _______,    _______, _______
     ),
 
