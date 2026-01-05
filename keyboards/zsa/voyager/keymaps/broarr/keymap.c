bcd702aaeac78ed3bd6a40290a72be346e87bae1
bradford roarr <broarr@tuta.com>
2026-01-04 21:49:43 -0500

    adding chordal homerow stuff to try and optimize how these things work

--- keyboards/zsa/voyager/keymaps/broarr/keymap.c
+++ keyboards/zsa/voyager/keymaps/broarr/keymap.c
@@ -16,7 +16,7 @@ enum tap_dance_keycodes {
 
 tap_dance_action_t tap_dance_actions[] = {
     [EQ_LAYER0] = ACTION_TAP_DANCE_LAYER_MOVE(KC_EQL, 0),
-    [EQ_LAYER5] = ACTION_TAP_DANCE_LAYER_MOVE(KC_GRV, 5),
+    [EQ_LAYER5] = ACTION_TAP_DANCE_LAYER_MOVE(KC_EQL, 5),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -45,6 +45,15 @@ void housekeeping_task_user(void) {
     }
 }
 
+const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
+    LAYOUT(
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+        'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R', 
+                            '*', '*',  '*', '*'
+    );
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /**********************************************************************************************************
      *  LAYER 0 LAYOUT                                                                                        *
@@ -69,11 +78,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      **********************************************************************************************************/
 
     [0] = LAYOUT(
-        KC_ESC,  KC_1,         KC_2,         KC_3,         KC_4,         KC_5, KC_6, KC_7,           KC_8,         KC_9,         KC_0,         KC_MINS,
-        KC_TAB, KC_Q,         KC_W,         KC_F,         KC_P,         KC_B, KC_J, KC_L,           KC_U,         KC_Y,         KC_SCLN,      KC_BSLS,
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
+        KC_TAB, KC_Q, KC_W, KC_F, KC_P, KC_B, KC_J, KC_L, KC_U, KC_Y, KC_SCLN, KC_BSLS,
         CW_TOGG, LGUI_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LCTL_T(KC_T), KC_G, KC_M, RCTL_T(KC_N),   RSFT_T(KC_E), RALT_T(KC_I), RGUI_T(KC_O), KC_QUOT,
-        TD(EQ_LAYER5),  KC_Z,         KC_X,         KC_C,         KC_D,         KC_V, KC_K, KC_H,KC_COMMA,KC_DOT,KC_SLSH,KC_EQL,
-                                             LT(2,KC_ENT), LT(1,KC_TAB),            LT(4,KC_BSPC), LT(3,KC_SPC)
+        TD(EQ_LAYER5), KC_Z, KC_X, KC_C, KC_D, KC_V, KC_K, KC_H, KC_COMMA, KC_DOT, KC_SLSH, KC_EQL,
+        LT(2,KC_ENT), LT(1,KC_TAB), LT(4,KC_BSPC), LT(3,KC_SPC)
     ),
 
     /**********************************************************************************************************
@@ -159,7 +168,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      **********************************************************************************************************/
 
     [3] = LAYOUT(
-        RGB_TOG, QK_KB,   RGB_MOD, RGB_M_P, RGB_VAD, RGB_VAI,                      _______, _______, _______, _______, _______, QK_BOOT,
+        UG_TOGG, QK_KB,   _______, RGB_M_P, _______, _______,                      _______, _______, _______, _______, _______, QK_BOOT,
         _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      _______, KC_HOME, KC_PGUP, _______,  _______, _______,
         _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                      _______, KC_END,  KC_PGDN, _______, _______, _______,
@@ -189,7 +198,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      **********************************************************************************************************/
 
     [4] = LAYOUT(
-        RGB_TOG, QK_KB,   RGB_MOD, RGB_M_P, RGB_VAD, RGB_VAI,                      _______, _______, _______, _______, _______, QK_BOOT,
+        UG_TOGG, QK_KB,   _______, RGB_M_P, _______, _______,                      _______, _______, _______, _______, _______, QK_BOOT,
         _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                      KC_PGUP, KC_HOME, KC_UP,   KC_END,  _______, _______,
         _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                      KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
         _______, _______, _______, _______, _______, _______,                      _______, C(S(KC_TAB)), C(KC_TAB), _______, _______, _______,
@@ -219,10 +228,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      **********************************************************************************************************/
 
     [5] = LAYOUT(
-        KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
-        KC_TAB, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTL,KC_A, KC_S, KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_Z,KC_X, KC_C,    KC_V,    KC_B,                         KC_N,    KC_M,    KC_COMMA,KC_DOT,  KC_SLSH,TD(EQ_LAYER0),
-                                   KC_SPC,  _______,          _______, _______
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+        KC_LCTL,KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_Z,KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH,TD(EQ_LAYER0),
+        KC_SPC, _______, _______, _______
     ),
 };
