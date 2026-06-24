4dbdb60363962e36e3a67dc8c722cecca8a82f5b
bradford roarr <bradford_roarr@brown.edu>
2026-06-24 17:08:23 -0400

    add f keys to gaming layout

--- keyboards/zsa/voyager/keymaps/broarr/keymap.c
+++ keyboards/zsa/voyager/keymaps/broarr/keymap.c
@@ -166,6 +166,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
         KC_LCTL,KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMMA, KC_DOT, KC_SLSH,TD(EQ_LAYER0),
-        KC_SPC, _______, _______, _______
+        KC_SPC, _______, _______, LT(1, KC_SPC)
     ),
 };
